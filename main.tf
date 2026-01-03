df0ed54a36ecd14fad2ae08180ac1df1a8c52345
neamanahmed <neaman.ahmed@gmail.com>
2026-01-03 16:04:07 +0400

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "me-central-1"
+}
+
+# RDS instance (MySQL)
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  identifier           = "eurodb"
+  db_name              = "dxbeuro"
+  engine               = "mysql"
+  engine_version       = "8.0.42"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "ItsN*$ou" # Hardcoded only for testing
+  parameter_group_name = "default.mysql8.0"
+
+  skip_final_snapshot    = true
+  publicly_accessible    = true # So you can connect for testing
+  vpc_security_group_ids = ["sg-0e4b168e4c00fdbc8"]
+  db_subnet_group_name   = "ae_rds_sbnet_grp"
+  tags = {
+    Name        = "dxbeuro-db"
+    Environment = "dev"
+  }
+
+
+}
+
+# Output the RDS endpoint
+output "rds_endpoint" {
+  value = aws_db_instance.default.address
+}
